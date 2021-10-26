21e1b88439b69ee8e397929c20075a9695634530
itoggi03 <itoggi03@gmail.com>
2021-10-26 23:47:23 +0900

    feat: 공공데이터 api 요청 및 응답

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 
-function Contents() {
+const Contents = () => {
+  useEffect(() => {
+    const baseUrl = "openapi/service/rest/Covid19/getCovid19SidoInfStateJson";
+    const serviceKey =
+      "HcvseZAI2To4xVKAVTkYti5g0ixy97Dpnjk5GtVKvJzwEgmTtwYVtAk8PFlX545ZLisNwrFncXKF%2FEufGVRT%2Fg%3D%3D";
+
+    const res = axios.get(baseUrl + "?ServiceKey=" + serviceKey).then((res) => {
+      console.log(res.data);
+    });
+  });
+
   return (
     <section className="section">
       <h2>국내 코로나 현황</h2>
       <div className="contents"></div>
     </section>
   );
-}
+};
 
 export default Contents;
